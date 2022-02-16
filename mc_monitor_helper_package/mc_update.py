274f0cf834829127223ba75740272cb125b89471
Gitznik <r.offner@gmx.de>
2022-02-16 17:23:09 +0100

    Fix setting monitors

--- mc_monitor_helper_package/mc_update.py
+++ mc_monitor_helper_package/mc_update.py
@@ -1,27 +1,34 @@
-from typing import Dict, List
+from typing import Any, Dict, List
 
 from mc_monitor_helper_package.api_functions import query_mc_api
 from mc_monitor_helper_package.auth import MonteCarloAuth
 from mc_monitor_helper_package.mc_table import MonteCarloTableContext
-from mc_monitor_helper_package.queries import MonitorSetter
+from mc_monitor_helper_package.queries import CreateOrUpdateMonitorSetter
 
 
 def set_monitor(
     auth: MonteCarloAuth,
     table_with_context: MonteCarloTableContext,
     fields: List[str],
+    fields_to_ignore: List[str],
     time_field: str,
     monitor_type: str = "stats",
     schedule_config: Dict = {"scheduleType": "LOOSE", "intervalMinutes": 720},
-) -> None:
-    query_mc_api(
+) -> Dict[Any, Any]:
+    time_field = str.lower(time_field)
+    fields_to_ignore = [str.lower(field) for field in fields_to_ignore]
+    fields_filtered = [field for field in fields if field not in fields_to_ignore]
+    return query_mc_api(
         auth=auth,
-        executable=MonitorSetter(
+        executable=CreateOrUpdateMonitorSetter(
             mcon=table_with_context.mcon,
-            fields=fields,
-            time_axis_type=table_with_context.timefields[time_field],
+            fields=fields_filtered,
+            time_axis_type=table_with_context.fields[time_field],
             time_axis_name=time_field,
+            aggregation_time_interval="HOUR",
+            lookback_days=1,
             monitor_type=monitor_type,
             schedule_config=schedule_config,
+            uuid=table_with_context.monitor_id,
         ),
     )
