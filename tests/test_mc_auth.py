2f7aa2d4a07f1e095fe9cf3ab6a1111a615b0f79
Gitznik <r.offner@gmx.de>
2022-02-18 10:59:52 +0100

    Add test for auth from args

--- tests/test_mc_auth.py
+++ tests/test_mc_auth.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
-from mc_monitor_helper_package.auth import get_monte_carlo_auth_from_env
+from mc_monitor_helper_package.auth import (
+    get_monte_carlo_auth,
+    get_monte_carlo_auth_from_env,
+)
 
 load_dotenv()
 
@@ -15,3 +18,9 @@ def test_get_auth_headers():
     headers = auth.auth_headers
     assert headers["x-mcd-id"] == "X_MCD_ID"
     assert headers["x-mcd-token"] == "X_MCD_TOKEN"
+
+
+def test_get_auth_from_args():
+    auth = get_monte_carlo_auth("X_MCD_ID", "X_MCD_TOKEN")
+    assert auth.x_mcd_id == "X_MCD_ID"
+    assert auth.x_mcd_token == "X_MCD_TOKEN"
